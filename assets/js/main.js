0bc7b3591b01bbf8a7231574e09656dc59e727cb
Stanni11 <supercoder@SuperCoders-MacBook-Air.local>
2022-07-19 19:04:00 +0200

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,20 @@
+
+function greenPurple() {
+    document.getElementById('body').style.background = 'rgb(0, 139, 90)'
+    document.getElementById('h1').style.color = 'rgb(161, 36, 250)'
+}
+
+function redYellow() {
+    document.getElementById('body').style.background = 'rgb(196, 16, 40)'
+    document.getElementById('h1').style.color = 'rgb(255, 200, 0)'
+}
+
+function greenBrown() {
+    document.getElementById('body').style.background = 'rgb(0, 255, 72)'
+    document.getElementById('h1').style.color = 'rgb(130, 21, 21)'
+}
+
+function blueYellow() {
+    document.getElementById('body').style.background = ' rgb(0, 251, 255)'
+    document.getElementById('h1').style.color = 'rgb(227, 238, 13)'
+}
